cfe1269a65a49db04b59197f839056e9d8f48049
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2026-09-10 06:58:11 +0000

    feat: add OriginalAudioInvalidException domain exception

---
+++ backend/src/main/java/com/lasono/track/domain/audio/exception/OriginalAudioInvalidException.java
@@ -0,0 +1,8 @@
+package com.lasono.track.domain.audio.exception;
+
+public class OriginalAudioInvalidException extends RuntimeException {
+
+    public OriginalAudioInvalidException(String message) {
+        super(message);
+    }
+}
