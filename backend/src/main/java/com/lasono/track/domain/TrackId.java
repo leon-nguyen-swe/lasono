25cd530220538946a4da3743c1f03cd47e21f9d7
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2026-09-10 06:58:34 +0000

    feat: add TrackId value object

---
+++ backend/src/main/java/com/lasono/track/domain/TrackId.java
@@ -0,0 +1,27 @@
+package com.lasono.track.domain;
+
+import java.util.UUID;
+import java.util.Objects;
+
+public class TrackId {
+
+    private final UUID id;
+
+    public TrackId(UUID id) {
+        this.id = Objects.requireNonNull(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof TrackId) {
+            TrackId other = (TrackId) o;
+            return this.id.equals(other.id);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
