feaeae8f70c44a22dba0b9c13c85ffaa742348a5
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2026-09-10 06:58:08 +0000

    feat: add AudioDurationInvalidException domain exception

---
+++ backend/src/main/java/com/lasono/track/domain/audio/exception/AudioDurationInvalidException.java
@@ -0,0 +1,8 @@
+package com.lasono.track.domain.audio.exception;
+
+public class AudioDurationInvalidException extends RuntimeException {
+
+    public AudioDurationInvalidException(String message) {
+        super(message);
+    }
+}
