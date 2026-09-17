04309a09c4a46fa8f80c1f4f4a4532d8433ec742
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2026-09-17 11:58:35 +0000

    feat: add AudioFormatInvalidException for handling invalid audio format errors

---
+++ backend/src/main/java/com/lasono/track/domain/audio/exception/AudioFormatInvalidException.java
@@ -0,0 +1,8 @@
+package com.lasono.track.domain.audio.exception;
+
+public class AudioFormatInvalidException extends RuntimeException {
+
+    public AudioFormatInvalidException(String message) {
+        super(message);
+    }
+}
