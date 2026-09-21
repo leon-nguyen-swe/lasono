a000353dcb93da30647a8384bac3b5744f542eb8
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2026-09-21 11:40:20 +0000

    feat: add unit tests for retrieveRange method in FakeAudioStorage

--- backend/src/test/java/com/lasono/track/application/usecase/FakeAudioStorage.java
+++ backend/src/test/java/com/lasono/track/application/usecase/FakeAudioStorage.java
@@ -19,6 +19,11 @@ class FakeAudioStorage implements AudioStorage {
     }
 
     @Override
+    public InputStream retrieveRange(StorageKey key, long offset, long length) {
+        return new ByteArrayInputStream(new byte[0]);
+    }
+
+    @Override
     public void delete(StorageKey key) {
         // no-op
     }
